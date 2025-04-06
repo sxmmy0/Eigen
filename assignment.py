258359d2b8cf7b7a099e2f5c891c470673c219bf
Samuel Obembe <samuel.obembe97@gmail.com>
2025-04-06 19:35:55 +0100

    Created code to process text to remove punctuation

--- assignment.py
+++ assignment.py
@@ -25,7 +25,35 @@ def load_text_files(directory: str):
     return text_files
 
 # Test the load_text_files function
-directory = "/Users/sxmmy/Documents/Projects/Eigen/Eigen"
+directory = "/Users/sxmmy/Documents/Projects/Eigen/Eigen" # Replace with your directory path
 text_files = load_text_files(directory)
  # print(text_files)
 
+# Step 2: Process the text to remove punctuation
+def process_text(text: str):
+    """
+    Remove punctuation from the text and return the cleaned text.
+    
+    Args:
+        text (str): The original text.
+        
+    Returns:
+        str: The cleaned text without punctuation.
+    """
+    # Remove '\n' 
+    text = text.replace('\n', ' ').replace('\\', '').replace('\'', '')
+
+    # Remove extra spaces
+    text = ' '.join(text.split())
+
+    # Convert to lowercase and remove punctuation
+    text = text.lower()
+    text = ''.join([char for char in text if char not in string.punctuation])
+
+    return text
+
+# Test the process_text function
+processed_text = process_text(text_files['doc6.txt']) # Replace with each document name
+
+# Print the processed text
+print(processed_text)
